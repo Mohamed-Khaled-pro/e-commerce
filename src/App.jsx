2517377a7c2ac6b2099aaf41d5dd76e72510b62c
Mohamed-Khaled-pro <mohamed.khaled.pro1@gmail.com>
2025-09-17 18:29:27 +0300

    Set Up Routing with react-router:

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { Route , Routes } from "react-router" 
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import LoginPage from "./pages/LoginPage"
+import RegisterPage from "./pages/RegisterPage"
+import Wishlist from "./pages/Wishlist"
+import Recommended from "./pages/Recommended"
+import Profile from "./pages/Profile"
+import MealsPage from "./pages/MealsPage"
+import MealDetails from "./pages/MealDetails"
+import Error from "./pages/Error"
 export default function App() {
   return (
-    <div className="bg-orange-500 text-white p-6 rounded">
-      <h1>    Hello Tailwind with custom colors!</h1>
-      <button className="btn btn-secondary">Go</button>
-    </div>
+  <>
+ <Navbar/>
+  <Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<About />} />
+  <Route path="/login" element={<LoginPage />} />
+  <Route path="/register" element={<RegisterPage />} />
+  <Route path="/profile" element={<Profile />} />
+  <Route path="/recommended" element={<Recommended />} />
+  <Route path="/wishlist" element={<Wishlist />} />
+  <Route path="/meals/:category" element={<MealsPage />} />
+  <Route path="/meal/:name" element={<MealDetails />} />
+  <Route path="*" element={<Error />} />
+  </Routes>
+  </>
   )
 }
