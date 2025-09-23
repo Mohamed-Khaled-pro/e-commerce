b2bcca58f1b8cdb494ac157875ba7a3ad0e78a14
Mohsen Hesham <mohsenhesham.110@gmail.com>
2025-09-23 04:08:23 +0300

    done the error page and tested the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,55 @@
-import { Route, Routes } from "react-router";
+import { Routes, Route } from "react-router";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+// Components
 import Navbar from "./components/Navbar";
-import Contact from "./pages/Contact";
+
+// Pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Profile from "./pages/Profile";
-import MealsPage from "./pages/MealsPage";
-import MealDetails from "./pages/MealDetails";
-import Error from "./pages/Error";
 import Favourites from "./pages/Favourites";
 import Reviews from "./pages/Reviews";
+import MealsPage from "./pages/MealsPage";
+import MealDetails from "./pages/MealDetails";
 import Category from "./pages/Category";
+import Error from "./pages/Error";
 
 export default function App() {
   return (
     <>
+     
       <Navbar />
+
+    
       <Routes>
+       
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+        
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+
         <Route path="/profile" element={<Profile />} />
         <Route path="/favourites" element={<Favourites />} />
         <Route path="/reviews" element={<Reviews />} />
+
         
-        {/* Separate routes with different patterns */}
+        <Route path="/meals" element={<Category />} />
         <Route path="/meals/category/:category" element={<MealsPage />} />
         <Route path="/meals/area/:area" element={<MealsPage />} />
-        <Route path="/meals" element={<Category />} />
-        
         <Route path="/meal/:id" element={<MealDetails />} />
-        <Route path="/contact" element={<Contact />} />
+
+        
         <Route path="*" element={<Error />} />
       </Routes>
+
       <ToastContainer position="top-center" autoClose={3000} />
     </>
   );
--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
-export default function Error() {
+export default function ErrorPage() {
   return (
-    <div>Error</div>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-6xl font-bold text-orange-700">404</h1>
+      <p className="mt-4 text-xl text-gray-700">Oops! Page not found.</p>
+      <Link 
+        to="/" 
+        className="mt-6  btn btn-primary"
+      >
+        Go Home
+      </Link>
+    </div>
+  );
 }
